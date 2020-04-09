bf8b333915584e74f18c726b98e7ba94f2f6cae6
uhcoronaa <uhcorona@gmail.com>
2020-04-09 17:04:12 -0600

    backend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '54.161.210.216',
+  user     : 'root',
+  password : 'lemus540',
+  database : 'intermedias'
+});
+connection.connect();
+
+
+
+
+app.get('/', (req, res) => {
+  connection.query('select * from USUARIO', function(err, rows, fields) {
+    if (!err){
+      res.send(rows)
+    } 
+    else{
+      throw err;
+    }
+    //res.send(rows);
+  });
+});
+
+app.listen(3000, () => console.log('server started'));
